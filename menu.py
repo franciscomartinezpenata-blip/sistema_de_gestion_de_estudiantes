d64753b1fcb025b19e6fab906b11e3e54776647e
franciscomartinezpenata-blip <franciscomartinezpenata@gmail.com>
2026-03-30 15:08:09 -0500

    Create menu.py
    The function to print the menu options was created to be called in the main function, thus determining the next step.

---
+++ menu.py
@@ -0,0 +1,10 @@
+def menu_de_opciones():
+   
+    print("1.registrar un nuevo estudiante")
+    print("2.consultar lista de estudiantes")
+    print("3.buscar estudiante con id o nombre")
+    print("4.actualizar informacion de un estudiante")
+    print("5.eliminar estudiantes")
+    int(input("escoga la opcion que desea realizar"))
+
+menu_de_opciones()
