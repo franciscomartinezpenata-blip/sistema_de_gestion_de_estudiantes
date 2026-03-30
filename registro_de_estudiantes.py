bece4f51bbefaabe575450197b0c9047221c4eb3
franciscomartinezpenata-blip <franciscomartinezpenata@gmail.com>
2026-03-30 14:36:59 -0500

    Create registro_de_estudiantes.py
    se creo la funcion de registro de estudiantes para luego ser llamada en el archivo main aun sigue sujeta a cambios

---
+++ registro_de_estudiantes.py
@@ -0,0 +1,6 @@
+def registro_de_estudiantes():
+    print ("ingrese los datos")
+    ID=input ("ingrese el numero indentificador ")
+    nombre=input ("nombre del nuevo estudiante ")
+    edad= int(input("ingrese la edad del nuevo estuante "))
+registro_de_estudiantes()
